247556e71c7241c4cb92ba74964a4f04bfceea94
BB808B <bli852039@gmail.com>
2025-07-26 18:33:07 +0630

    Create UserProvider.tsx

---
+++ frontend/src/providers/UserProvider.tsx
@@ -0,0 +1,17 @@
+// src/providers/UserProvider.tsx
+'use client';
+import { MyUserContextProvider } from "@/hooks/useUser";
+
+interface UserProviderProps {
+    children: React.ReactNode;
+}
+
+const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+    return (
+        <MyUserContextProvider>
+            {children}
+        </MyUserContextProvider>
+    )
+}
+
+export default UserProvider;
